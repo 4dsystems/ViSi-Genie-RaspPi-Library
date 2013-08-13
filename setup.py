16e41acc32cd198e4e1bfb05e210a13473429425
David Michel <dmichel76@googlemail.com>
2013-08-13 13:53:09 +0100

    added setup.py for easy installation of Python module

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='geniePi',
+      version='1.0',
+      description='Python bindings for the ViSi-Genie-RaspPi-Library, a C library for interfacing 4D Systems displays with a Raspberry Pi',
+      author='David Michel',
+      author_email='dmichel76@googlemail.com',
+      url='https://github.com/dmichel76/ViSi-Genie-RaspPi-Library',
+      py_modules=['geniePi'],
+     )
